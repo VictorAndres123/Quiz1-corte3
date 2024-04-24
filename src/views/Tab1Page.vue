fb9766a05532cea00c4024afb17ec7e7411c1643
VictorAndres123 <vrojas-2022a@corhuila.edu.co>
2024-04-24 08:21:48 -0500

    Quiz del corte 3

---
+++ src/views/Tab1Page.vue
@@ -0,0 +1,59 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Tienda Online</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Tab 1</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <div id="login-form">
+          <InputComponent id="user" name="Nombre" label="Nombre: " />
+          <InputComponent type="number" id="tel" name="Celular" label="Celular: " />
+          <InputComponent type="email" id="email" name="correo" label="Email: " />
+          <InputComponent type="date" id="fecha" name="Fecha_de_Nacimiento" />
+          
+
+          <br />
+
+          <ButtonComponent id="consult" value="Consultar" fill="outline" />
+          <ButtonComponent id="save" value="Agregar" fill="outline" color="success" />
+          <ButtonComponent id="update" value="Modificar" fill="outline" color="warning" />
+          <ButtonComponent id="delete" value="Eliminar" fill="outline" color="danger" />
+        
+        </div>
+      
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonHeader, IonToolbar,IonButton, IonTitle, IonContent } from '@ionic/vue';
+import ExploreContainer from '@/components/ExploreContainer.vue';
+import ButtonComponent from '@/components/ButtonComponent.vue';
+import InputComponent from '@/components/InputComponent.vue'
+</script>
+<style scoped>
+#login-form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 90vh; /* Usar altura completa de la ventana */
+  padding: 20px; /* Añadir relleno para separar los elementos */
+}
+
+#login-form ion-item {
+  width: 100%; /* Ajustar el ancho de los elementos de entrada */
+  margin-bottom: 10px; /* Agregar espacio entre elementos */
+}
+
+#login-form ion-button {
+  width: 50%; /* Ajustar el ancho de los botones */
+  margin-bottom: 30px; /* Agregar espacio entre botones */
+}
+</style>
---
+++ src/views/Tab2Page.vue
@@ -0,0 +1,43 @@
+<template>
+  
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Title</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Tab 2</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content class="ion-padding">
+    <h1>Heading 1</h1>
+    <h2>Heading 2</h2>
+    <h3>Heading 3</h3>
+    <h4>Heading 4</h4>
+    <h5>Heading 5</h5>
+    <h6>Heading 6</h6>
+
+    <p>Here's a small text description for the content. Nothing more, nothing less.</p>
+  </ion-content>
+  <ion-footer>
+    <ion-toolbar>
+      <ion-title> Footer </ion-title>
+    </ion-toolbar>
+  </ion-footer>
+
+
+
+      
+    </ion-content>
+</template>
+
+<script lang="ts">
+  import { IonContent, IonFooter, IonHeader, IonTitle, IonToolbar } from '@ionic/vue';
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    components: { IonContent, IonFooter, IonHeader, IonTitle, IonToolbar },
+  });
+</script>
